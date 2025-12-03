a90beb9a7a6b1b9cae87a059d4a158e0bf36b685
di <dibavakil@gmail.com>
2025-12-03 17:26:50 +0300

    Добавлены комментарии к утилитам для работы с CSS классами, описывающие их функциональность и разрешение конфликтов Tailwind.

--- cat-parrot-karaoke/src/lib/utils.ts
+++ cat-parrot-karaoke/src/lib/utils.ts
@@ -1,3 +1,7 @@
+/**
+ * Утилиты для работы с CSS классами. 
+ * Объединяет CSS классы с разрешением конфликтов Tailwind (clsx + tailwind-merge)
+ */
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
